0f4815dee59e7887f3c400d49150a8525401ae04
Mafouz DIALLO <46787062+fouzo09@users.noreply.github.com>
2024-01-13 09:06:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+    host: 'db_server_host',
+    user: 'mysql_username',
+    password: 'mysql_password',
+    database: 'db_name'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('MySQL connection error: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as id ' + db.threadId);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
+    db.query(sql, [name, email], (err, result) => {
+        if (err) throw err;
+        res.json({ message: 'User created successfully', id: result.insertId });
+    });
+});
+
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
